545aa685134cfa2f898dd1fe1e9d00c66e3162b0
cqboyone <cqboyone@163.com>
2021-08-23 11:14:19 +0800

    优化存储更新逻辑

--- crawler/src/main/java/com/vv/tool/film/assistant/crawler/webmagic/pipeline/MoviePipeline.java
+++ crawler/src/main/java/com/vv/tool/film/assistant/crawler/webmagic/pipeline/MoviePipeline.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.vv.tool.film.assistant.crawler.module.entity.MovieCollect;
 import com.vv.tool.film.assistant.crawler.module.service.MovieCollectService;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
@@ -25,22 +26,19 @@ public class MoviePipeline implements Pipeline {
         List<String> rate = resultItems.get("rate");
         for (int i = 0; i < movies.size(); i++) {
             String movieName = movies.get(i);
+            if(StringUtils.isBlank(movieName)){
+                continue;
+            }
             //过滤评分
             double score = Double.parseDouble(rate.get(i));
             if (score < 7) {
                 continue;
             }
-            MovieCollect one = movieCollectService.getOne(new LambdaUpdateWrapper<MovieCollect>()
+            MovieCollect movieCollect = new MovieCollect();
+            movieCollect.setMovieName(movieName);
+            movieCollect.setMovieScore(score);
+            movieCollectService.saveOrUpdate(movieCollect, new LambdaUpdateWrapper<MovieCollect>()
                     .eq(MovieCollect::getMovieName, movieName));
-            if (one == null) {
-                MovieCollect movieCollect = new MovieCollect();
-                movieCollect.setMovieName(movieName);
-                movieCollect.setMovieScore(score);
-                movieCollectService.save(movieCollect);
-            } else {
-                one.setMovieScore(score);
-                movieCollectService.updateById(one);
-            }
         }
     }
 }
